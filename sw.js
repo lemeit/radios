3bc8715eaaa94aebd23087f8dfc6003674b57310
Luciano Lamaita <63206725+lemeit@users.noreply.github.com>
2026-04-30 07:36:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'miradio-v1';
+const CACHE = 'miradio-v4';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
